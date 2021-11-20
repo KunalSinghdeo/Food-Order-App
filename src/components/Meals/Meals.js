f3045b6c8f2098b9b393e84ad2de27122bb39309
Kunal Singhdeo <singhdeokunal11@gmail.com>
2021-11-20 23:46:22 +0530

    Meals Component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <AvailableMeals />
+      <MealsSummary />
+    </Fragment>
+  );
+};
+export default Meals;
