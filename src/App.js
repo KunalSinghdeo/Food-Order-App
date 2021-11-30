7c0a4c02c955da5a6f3a2501ba3b6025c8754667
Kunal Singhdeo <singhdeokunal11@gmail.com>
2021-11-30 20:04:16 +0530

    cart modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    return setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    return setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
