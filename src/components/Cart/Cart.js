6a13519f9ad2b3e11521d97cbe0499ba5ebc9c68
Kunal Singhdeo <singhdeokunal11@gmail.com>
2021-12-01 21:19:13 +0530

    cart output

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,30 @@
+import { useContext } from "react";
+
+import CartItem from "./CartItem";
+import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `${cartCtx.totalAmount.toFixed(2)} ₹`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <l1>{item.name}</l1>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,3 @@
-// import classes from "./Card.module.css";
-
-// const Card = (props) => {
-//   return <div className={classes.card}>{props.children}</div>;
-// };
-// export default Card;
-
 import classes from "./Card.module.css";
 
 const Card = (props) => {
